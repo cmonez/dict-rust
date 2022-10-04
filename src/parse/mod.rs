146dc3f4107122a8b36684032b1878d0925f3732
Eric Zepeda <zepedaeric@gmail.com>
2022-10-03 21:19:34 -0700

    Continue work on fetching elements

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,7 +1,12 @@
-use scraper::*;
+use scraper::{Html, Selector};
+
+const DICTIONARY_CLASS_CONTAINER: &str = "css-1avshm7";
 
 pub mod parse {
+    use super::*;
+
     pub fn get_definition(html_string: &str) {
-        let document = scraper::Html::parse_document(html_string);
+        let document: Html = Html::parse_document(html_string);
+        let selector = Selector::parse(format!("div.{}", DICTIONARY_CLASS_CONTAINER).as_str()).unwrap();
     }
 }
