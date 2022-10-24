be9a8fa4dc8f71f686f2805c36c983003bb5e158
Eric Zepeda <zepedaeric@gmail.com>
2022-10-24 00:15:32 -0700

    Continue work on rendering definitions

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use fetch::fetch::*;
 fn main() {
     let args = args::Args::parse();
     let word = args.word;
-    println!("Word {}", &word);
-    println!("Synonym {}", args.synonym);
-    println!("Antonym {}", args.antonym);
+    // println!("Word {}", &word);
+    // println!("Synonym {}", args.synonym);
+    // println!("Antonym {}", args.antonym);
     get_definition(&word);
     get_synonym(&word);
     get_antonym(&word);
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,7 +1,7 @@
 use scraper::{Html, Selector};
 
-const DICTIONARY_SELECTOR_ONE: &str = "css-1avshm7";
-const DICT_SELECTOR_TWO: &str = "e16867sm0";
+const TOP_LEVEL_DICTIONARY_DIV_SELECTOR: &str = ".css-1avshm7, .e16867sm0";
+const PART_OF_SPEECH_SECTION_SELECTOR: &str = ".css-109x55k, .e1hk9ate4";
 
 pub mod parse {
     use scraper::ElementRef;
@@ -9,13 +9,26 @@ pub mod parse {
     use super::*;
 
     pub fn get_definition(html_string: &str) {
-        let mut html_div = get_definition_html_div(html_string);
-        println!("Here is the html div {}", html_div)
+        let mut html_div = get_top_level_definition_html_div(html_string);
+        get_defintions(html_div.as_str());
     }
 
-    fn get_definition_html_div(html_string: &str) -> String {
+    fn get_top_level_definition_html_div(html_string: &str) -> String {
         let mut document: Html = Html::parse_document(html_string);
-        let selector = Selector::parse(format!(".{}, .{}", DICTIONARY_SELECTOR_ONE, DICT_SELECTOR_TWO).as_str()).unwrap();
+        let selector = Selector::parse(TOP_LEVEL_DICTIONARY_DIV_SELECTOR).unwrap();
         document.select(&selector).next().unwrap().inner_html()
+    }
+    fn get_defintions(html_string: &str)  {
+        let mut document: Html = Html::parse_document(html_string);
+        let selector = Selector::parse(PART_OF_SPEECH_SECTION_SELECTOR).unwrap();
+        document.select(&selector).for_each(|item| {
+            item.text().for_each(|text| {
+                match &text[..1] {
+                    "." => print!(""),
+                    " " => print!(""),
+                    _   => print!("{} ", text)
+                }
+            })
+        })
     }
 }
